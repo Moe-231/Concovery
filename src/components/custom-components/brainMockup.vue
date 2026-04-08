9d16ee11b373bbf8f270f6db95140dc32b59c903
Moe-231 <cmut0006@student.monash.edu>
2026-04-08 18:38:10 +1000

    Home Page Brain Design Updated

--- src/components/custom-components/brainMockup.vue
+++ src/components/custom-components/brainMockup.vue
@@ -27,6 +27,12 @@ const div4Ref = useTemplateRef("div4Ref");
 const div5Ref = useTemplateRef("div5Ref");
 const div6Ref = useTemplateRef("div6Ref");
 const div7Ref = useTemplateRef("div7Ref");
+const ParietalRef = useTemplateRef("ParietalRef")
+const OcipitalRef = useTemplateRef("OcipitalRef")
+const Cerebellum = useTemplateRef("Cerebellum")
+const Brainstem = useTemplateRef("Brainstem")
+
+
 </script>
 
 <template>
@@ -37,10 +43,10 @@ const div7Ref = useTemplateRef("div7Ref");
       <div class="flex flex-row items-center justify-between">
         <!-- Parietal Lobe -->
         <div ref="div1Ref"
-          class="relative cursor-crosshair z-3 flex size-12 items-center justify-center rounded-full border-2 bg-red-500 p-2 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)] dark:text-black group">
-          <div class="absolute cardAnimation bg-red-200 hidden top-0 left-0 w-[200px] border-2 rounded-md border-l-red-500 border-t-0 border-b-0 border-r-0 p-2 shadow-xl group-hover:flex flex-col space-y-1">
+          class="relative cursor-crosshair z-3 flex size-12 items-center justify-center rounded-full border-2 bg-yellow-500 p-2 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)] dark:text-black group">
+          <div class="absolute cardAnimation bg-yellow-200 hidden top-0 left-0 w-[200px] border-2 rounded-md border-l-yellow-500 border-t-0 border-b-0 border-r-0 p-2 shadow-xl group-hover:flex flex-col space-y-1">
             <div>
-              <span class="text-red-500 font-bold">Paritel Lobe</span>
+              <span class="text-yellow-500 font-bold">Paritel Lobe</span>
             </div>
             <div>
               <span class="text-gray-400 text-[12px] tracking-tight">
@@ -49,7 +55,7 @@ const div7Ref = useTemplateRef("div7Ref");
             </div>
             <div>
               <span class="text-[13px] font-bold">Concussion Symptoms</span>
-              <ul class="list-disc marker:text-red-500 text-[11px]">
+              <ul class="list-disc marker:text-yellow-500 text-[11px]">
                 <li class="ml-3">
                   Dizziness
                 </li>
@@ -68,10 +74,10 @@ const div7Ref = useTemplateRef("div7Ref");
         </div>
         <!-- Ocipital Lobe -->
         <div ref="div5Ref"
-          class="z-4 relative flex size-12 items-center justify-center rounded-full border-2 bg-green-500 p-2 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)] group cursor-crosshair">
-          <div class="absolute cardAnimation bg-green-200 hidden top-0 right-0 w-[200px] border-2 rounded-md border-l-green-500 border-t-0 border-b-0 border-r-0 p-2 shadow-xl group-hover:flex flex-col space-y-1">
+          class="z-4 relative flex size-12 items-center justify-center rounded-full border-2 bg-pink-500 p-2 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)] group cursor-crosshair">
+          <div class="absolute cardAnimation bg-pink-200 hidden top-0 right-0 w-[200px] border-2 rounded-md border-l-pink-500 border-t-0 border-b-0 border-r-0 p-2 shadow-xl group-hover:flex flex-col space-y-1">
             <div>
-              <span class="text-green-500 font-bold">Ocipital Lobe</span>
+              <span class="text-pink-500 font-bold">Ocipital Lobe</span>
             </div>
             <div>
               <span class="text-gray-400 text-[12px] tracking-tight">
@@ -80,7 +86,7 @@ const div7Ref = useTemplateRef("div7Ref");
             </div>
             <div>
               <span class="text-[13px] font-bold">Concussion Symptoms</span>
-              <ul class="list-disc marker:text-green-500 text-[11px]">
+              <ul class="list-disc marker:text-pink-500 text-[11px]">
                 <li class="ml-3">
                   Blurred Vision
                 </li>
@@ -132,7 +138,22 @@ const div7Ref = useTemplateRef("div7Ref");
         </div>
         <div ref="div4Ref"
           class="z-1 flex size-48 lg:size-68 items-center justify-center rounded-full bg-transparent p-2 text-black shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)]">
+          <div class="relative">
           <img src="/brainMain.png" class="w-40 lg:w-62.5" width="250px" height="150px" />
+          <!-- Parietal Destination -->
+            <div class="absolute top-[20%] right-[25%]" ref="ParietalRef">
+            </div>
+          <!-- Ocipital Destination -->  
+            <div class="absolute top-[55%] right-[8%]" ref="OcipitalRef">
+            </div>
+          <!-- Cerebellum   -->
+           <div class="absolute bottom-[7%] right-[20%]" ref="Cerebellum">
+           </div>
+          <!-- Brainstem   -->  
+           <div class="absolute bottom-[5%] right-[38%]" ref="Brainstem">
+           </div>
+          </div>
+        
           <!-- <Icon
             icon="simple-icons:openai"
             width="190px important"
@@ -173,10 +194,10 @@ const div7Ref = useTemplateRef("div7Ref");
       <div class="flex flex-row items-center justify-between">
         <!-- Temporal Lobe -->
         <div ref="div3Ref"
-          class="z-1 relative flex size-12 items-center justify-center rounded-full border-2 bg-yellow-500 p-2 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)] dark:text-black cursor-crosshair group">
-          <div class="absolute cardAnimation bg-yellow-200 hidden bottom-0 left-0 w-[200px] border-2 rounded-md border-l-yellow-500 border-t-0 border-b-0 border-r-0 p-2 shadow-xl group-hover:flex flex-col space-y-1">
+          class="z-1 relative flex size-12 items-center justify-center rounded-full border-2 bg-green-500 p-2 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)] dark:text-black cursor-crosshair group">
+          <div class="absolute cardAnimation bg-green-200 hidden bottom-0 left-0 w-[200px] border-2 rounded-md border-l-green-500 border-t-0 border-b-0 border-r-0 p-2 shadow-xl group-hover:flex flex-col space-y-1">
             <div>
-              <span class="text-yellow-500 font-bold">Temporal Lobe</span>
+              <span class="text-green-500 font-bold">Temporal Lobe</span>
             </div>
             <div>
               <span class="text-gray-400 text-[12px] tracking-tight">
@@ -185,7 +206,7 @@ const div7Ref = useTemplateRef("div7Ref");
             </div>
             <div>
               <span class="text-[13px] font-bold">Concussion Symptoms</span>
-              <ul class="list-disc marker:text-yellow-500 text-[11px]">
+              <ul class="list-disc marker:text-green-500 text-[11px]">
                 <li class="ml-3">
                   Memory loss
                 </li>
@@ -204,10 +225,10 @@ const div7Ref = useTemplateRef("div7Ref");
         </div>
         <!-- Brainstem -->
         <div ref="div7Ref"
-          class="z-2 relative flex size-12 items-center justify-center rounded-full border-2 bg-pink-500 p-2 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)] cursor-crosshair group">
-          <div class="absolute cardAnimation bg-pink-200 hidden bottom-0 right-0 w-[200px] border-2 rounded-md border-l-pink-500 border-t-0 border-b-0 border-r-0 p-2 shadow-xl group-hover:flex flex-col space-y-1">
+          class="z-2 relative flex size-12 items-center justify-center rounded-full border-2 bg-red-500 p-2 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)] cursor-crosshair group">
+          <div class="absolute cardAnimation bg-red-200 hidden bottom-0 right-0 w-[200px] border-2 rounded-md border-l-red-500 border-t-0 border-b-0 border-r-0 p-2 shadow-xl group-hover:flex flex-col space-y-1">
             <div>
-              <span class="text-pink-500 font-bold">Brainstem</span>
+              <span class="text-red-500 font-bold">Brainstem</span>
             </div>
             <div>
               <span class="text-gray-400 text-[12px] tracking-tight">
@@ -216,7 +237,7 @@ const div7Ref = useTemplateRef("div7Ref");
             </div>
             <div>
               <span class="text-[13px] font-bold">Concussion Symptoms</span>
-              <ul class="list-disc marker:text-pink-500 text-[11px]">
+              <ul class="list-disc marker:text-red-500 text-[11px]">
                 <li class="ml-3">
                   Loss of consciousness
                 </li>
@@ -246,16 +267,16 @@ const div7Ref = useTemplateRef("div7Ref");
         <span class="text-[10px] text-[#86868b] font-medium">Frontal Lobe</span>
       </div>
       <div class="flex items-center gap-1.5">
-        <div class="w-2 h-2 rounded-full bg-red-500"></div>
+        <div class="w-2 h-2 rounded-full bg-yellow-500"></div>
         <span
           class="text-[10px] text-[#86868b] font-medium">Parietal Lobe</span>
       </div>
       <div class="flex items-center gap-1.5">
-        <div class="w-2 h-2 rounded-full bg-yellow-500"></div><span
+        <div class="w-2 h-2 rounded-full bg-green-500"></div><span
           class="text-[10px] text-[#86868b] font-medium">Temporal Lobe</span>
       </div>
       <div class="flex items-center gap-1.5">
-        <div class="w-2 h-2 rounded-full bg-green-500"></div><span
+        <div class="w-2 h-2 rounded-full bg-pink-500"></div><span
           class="text-[10px] text-[#86868b] font-medium">Occipital Lobe</span>
       </div>
       <div class="flex items-center gap-1.5">
@@ -263,20 +284,20 @@ const div7Ref = useTemplateRef("div7Ref");
           class="text-[10px] text-[#86868b] font-medium">Cerebellum</span>
       </div>
       <div class="flex items-center gap-1.5">
-        <div class="w-2 h-2 rounded-full bg-pink-500"></div><span
+        <div class="w-2 h-2 rounded-full bg-red-500"></div><span
           class="text-[10px] text-[#86868b] font-medium">Brainstem</span>
       </div>
     </div>
 
-    <AnimatedBeam :container-ref="containerRef" path-color="red" :from-ref="div1Ref" :to-ref="div4Ref" :curvature="-10"
+    <AnimatedBeam :container-ref="containerRef" path-color="yellow" :from-ref="div1Ref" :to-ref="ParietalRef" :curvature="100"
       :end-y-offset="-10" />
     <AnimatedBeam path-color="blue" :container-ref="containerRef" :from-ref="div2Ref" :to-ref="div4Ref" />
-    <AnimatedBeam path-color="yellow" :container-ref="containerRef" :from-ref="div3Ref" :to-ref="div4Ref" :curvature="10"
+    <AnimatedBeam path-color="green" :container-ref="containerRef" :from-ref="div3Ref" :to-ref="div4Ref" :curvature="10"
       :end-y-offset="10" />
-    <AnimatedBeam path-color="green" :container-ref="containerRef" :from-ref="div5Ref" :to-ref="div4Ref" :curvature="-10"
+    <AnimatedBeam path-color="pink" :container-ref="containerRef" :from-ref="div5Ref" :to-ref="OcipitalRef"
       :end-y-offset="-10" />
-    <AnimatedBeam path-color="purple" :container-ref="containerRef" :from-ref="div6Ref" :to-ref="div4Ref" />
-    <AnimatedBeam path-color="pink" :container-ref="containerRef" :from-ref="div7Ref" :to-ref="div4Ref" :curvature="10"
+    <AnimatedBeam path-color="purple" :container-ref="containerRef" :from-ref="div6Ref" :to-ref="Cerebellum" />
+    <AnimatedBeam path-color="red" :container-ref="containerRef" :from-ref="div7Ref" :to-ref="Brainstem"
       :end-y-offset="10" />
   </div>
   <div class="relative">
