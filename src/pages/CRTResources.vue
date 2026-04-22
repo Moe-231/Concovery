e93ed685aea4b39f07197f91c9bb6ab7b4a0f28f
Moe-231 <cmut0006@student.monash.edu>
2026-04-23 09:06:19 +1000

    Refactoring

--- src/pages/CRTResources.vue
+++ src/pages/CRTResources.vue
@@ -50,7 +50,8 @@ import BlurReveal from '@/components/ui/blur-reveal/BlurReveal.vue';
                                             </ul>
                                         </div>
                                     </div>
-                                </div><a data-slot="button" href="https://cattonline.com/" 
+                                </div>
+                                <a data-slot="button" href="https://cattonline.com/" target="_blank" 
                                     class="inline-flex items-center justify-center gap-2 whitespace-nowrap disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg:not([class*='size-'])]:size-4 shrink-0 [&amp;_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-10 has-[&gt;svg]:px-4 bg-white text-[#007aff] hover:bg-white/90 px-12 py-7 text-xl rounded-full font-semibold shadow-lg transition-all hover:scale-105">Access
                                     CRT5 Tool<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
